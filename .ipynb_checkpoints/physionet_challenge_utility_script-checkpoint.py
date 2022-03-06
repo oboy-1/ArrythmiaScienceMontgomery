3551ba9ea623650c71c4d4823f0dbe8e744cc81d
oboy-1 <orangegeoboy@gmail.com>
2022-03-06 15:42:30 -0500

    DatacClean

--- .ipynb_checkpoints/physionet_challenge_utility_script-checkpoint.py
+++ .ipynb_checkpoints/physionet_challenge_utility_script-checkpoint.py
@@ -226,7 +226,7 @@ def calculating_class_weights(y_true):
     number_dim = np.shape(y_true)[1]
     weights = np.empty([number_dim, 2])
     for i in range(number_dim):
-        weights[i] = compute_class_weight('balanced', [0.,1.], y_true[:, i])
+        weights[i] = compute_class_weight(class_weight='balanced', classes=[0.,1.], y=y_true[:, i])
     return weights
 
 
